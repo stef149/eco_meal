42b6c1a6acd9ec6c4b9f4e4c6bcefe05c2a804e9
Dina Stefan <dina.stefa.w8m@student.ucv.ro>
2026-07-08 18:29:05 +0300

    login Problem

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,26 +4,34 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 50)]
+    #[ORM\Column(length: 180)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 50)]
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
     private ?string $password = null;
-
-    #[ORM\OneToOne(mappedBy: 'user', cascade: ['persist', 'remove'])]
-    private ?Consumer $consumer = null;
-
-    #[ORM\OneToOne(mappedBy: 'user', cascade: ['persist', 'remove'])]
-    private ?Business $business = null;
 
     public function getId(): ?int
     {
@@ -42,6 +50,41 @@ class User
         return $this;
     }
 
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
     public function getPassword(): ?string
     {
         return $this->password;
@@ -54,47 +97,28 @@ class User
         return $this;
     }
 
-    public function getConsumer(): ?Consumer
+    /**
+     * Ensure the session doesn't contain actual password hashes by CRC32C-hashing them, as supported since Symfony 7.3.
+     */
+    public function __serialize(): array
     {
-        return $this->consumer;
+        $data = (array) $this;
+        $data["\0".self::class."\0password"] = hash('crc32c', $this->password);
+
+        return $data;
     }
 
-    public function setConsumer(?Consumer $consumer): static
+    #[\Deprecated]
+    public function eraseCredentials(): void
     {
-        // unset the owning side of the relation if necessary
-        if ($consumer === null && $this->consumer !== null) {
-            $this->consumer->setUser(null);
-        }
-
-        // set the owning side of the relation if necessary
-        if ($consumer !== null && $consumer->getUser() !== $this) {
-            $consumer->setUser($this);
-        }
-
-        $this->consumer = $consumer;
-
-        return $this;
+        // @deprecated, to be removed when upgrading to Symfony 8
     }
 
-    public function getBusiness(): ?Business
+    public function setConsumer(mixed $consumer)
     {
-        return $this->business;
     }
 
-    public function setBusiness(?Business $business): static
+    public function setBusiness(mixed $business)
     {
-        // unset the owning side of the relation if necessary
-        if ($business === null && $this->business !== null) {
-            $this->business->setUser(null);
-        }
-
-        // set the owning side of the relation if necessary
-        if ($business !== null && $business->getUser() !== $this) {
-            $business->setUser($this);
-        }
-
-        $this->business = $business;
-
-        return $this;
     }
 }
